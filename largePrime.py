042da62e4723ae5ed50f6057cf13ecf7777c5b8a
Tage <tage.hisdal@gmail.com>
2022-10-18 11:25:42 +0200

    Comments added

--- largePrime.py
+++ largePrime.py
@@ -1,27 +1,25 @@
 import random
 import time
 
-prime_candidate = 2359387295841603593872497896878874911354964981437788423001084377685944959437718053927835500869359979283068184632251708346109938684331052172298133038997763758508710452918070355313179108348018708020045284488899366383442690192063712113658805209824845665266206807184787493604374694215753343981094706853471784559
-
 def generate_odd_number():
-    big_number = random.getrandbits(1024)
-    if (big_number % 2) != 0:
+    big_number = random.getrandbits(1024) #Generate random 1024 bit number
+    if (big_number % 2) != 0: #Checks if number is odd
         odd = big_number
     else:
-        odd = big_number + 1
-    return odd
+        odd = big_number + 1 #If it's not odd, make it odd
+    return odd #Return odd number
 
 def miller_rabin(number_to_test, iterations):
-    u = number_to_test - 1
+    #Find u odd such that number_to_test-1 = 2^k * u 
+    u = number_to_test - 1 
     k = 0
+    while (u % 2 == 0): #While u is even
+        u //= 2 #Factoring out powers of 2 from number_to_test-1
+        k += 1 #k is numbers of factorings done 
 
-    while (u % 2 == 0):
-        u //= 2
-        k += 1
-    
     for _ in range(iterations):
-        a = random.randint(2, number_to_test-2) #math.sqrt(number_to_test) faster?
-        b = pow(a,u,number_to_test)
+        base = random.randint(2, number_to_test-2) #Generate random base to test with
+        b = pow(base,u,number_to_test) #b = base^u mod number_to_test 
 
         if (b == 1 or b == (number_to_test-1)):
             continue
@@ -35,13 +33,14 @@ def miller_rabin(number_to_test, iterations):
 
     return True
 
+
 iterations = 100
 i = 0
-tested_number = generate_odd_number()
+#tested_number = generate_odd_number()
 
 start_time = time.time()
 
-print(miller_rabin(prime_candidate, 50))
+print(miller_rabin(97,100))
 print("--- %s seconds ---" % (time.time() - start_time))
 
 '''
@@ -54,4 +53,5 @@ while i < 1000:
     tested_number = generate_odd_number()
     i += 1
 '''
+
 
